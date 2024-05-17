b1861ed6ef0b47ac108210a9c98e3bf163c353e3
marco-soria <marco.soriaa7@gmail.com>
2024-05-16 20:19:27 -0500

    documentation swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
+
 import { IncomingHttpHeaders } from 'http';
 
 import { AuthService } from './auth.service';
@@ -8,10 +10,10 @@ import { RoleProtected } from './decorators/role-protected.decorator';
 
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
-
-import { ValidRoles } from './interfaces';
 import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
